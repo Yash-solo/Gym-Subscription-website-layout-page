3a9a3099fd3520e9a4d8955cf945eb455f854c94
Yas-prog-blip <yashrajb916@gmail.com>
2026-06-18 08:40:54 +0530

    Add files via upload

---
+++ GYM.js
@@ -0,0 +1,8 @@
+function showSideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "flex";
+};
+function hideSidebar() {
+    const sidebar = document.getElementById('Sidebar');
+    sidebar.style.display = "none";
+};
